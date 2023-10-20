316d5d986ac72e5f4547658db0f2e2d1373a63ca
Samir Jadhav <samirzjadhav@gmail.com>
2023-10-20 19:53:08 +0530

    misc: learned aysnc and awits

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,18 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
 
 export default function Home() {
-  return (
+  const [advice, setAdvice] = useState("");
+  async function getAdvice() {
+    const res = fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
 
-  )
+  return (
+    <>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+    </>
+  );
 }
