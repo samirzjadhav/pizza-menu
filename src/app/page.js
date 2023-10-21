91ff4213a70be308d96f66d3c69ab23259792143
Samir Jadhav <samirzjadhav@gmail.com>
2023-10-21 22:35:03 +0530

    feat: added counter in adives fetch

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,23 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function App() {
   const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
   async function getAdvice() {
-    const res = fetch("https://api.adviceslip.com/advice");
+    const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
   }
-
   return (
-    <>
+    <div>
       <h1>{advice}</h1>
-      <button onClick={getAdvice}>Get Advice</button>
-    </>
+      <button onClick={getAdvice}>Get advice</button>
+      <p>
+        you have read <strong>{count}</strong> pieces of advice
+      </p>
+    </div>
   );
 }
